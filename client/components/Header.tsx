35ff01f68a8b5dd26a26b780ac5527c942e19289
yu23ki14 <yuki_021423@yahoo.co.jp>
2025-07-12 08:39:40 +0900

    ghp

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -6,7 +6,7 @@ import { HStack, Image } from "@chakra-ui/react";
 export function Header() {
   return (
     <HStack justify="space-between" py="5" mb={8} mx={"auto"} maxW={"1200px"}>
-      <Image src="/images/logo.png" alt="全員市長" w="180px" h="60px" />
+      <Image src="/images/logo.png" alt="全員市長" w="180px" h="90px" />
       <BroadlisteningGuide />
     </HStack>
   );
